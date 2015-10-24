0c4bd15700f3d9049c24c09546f987ed406a0b18
michael <mfalicea@buffalo.edu>
2015-10-24 22:32:45 +0000

    Filled in some more of the template

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,9 +1,23 @@
 class CandidatesController < ApplicationController
-  def berniesanders
-  	@candidateName = "Bernie Sanders"
-  end
-
   def genericcandidate
   	@candidateName = "This is a generic Candidate and will be removed once all the candidates are in place!"
+	@candidateBiography = "This is his biography!"
+	@speech1 = "https://www.youtube.com/embed/-oYIK6Bg6co"
+  	@speech2 = "https://www.youtube.com/embed/-oYIK6Bg6co"
+	@speech3 = "https://www.youtube.com/embed/-oYIK6Bg6co"
+	@debate1 = "https://www.youtube.com/embed/-oYIK6Bg6co"
+	@debate2 = "https://www.youtube.com/embed/-oYIK6Bg6co"
+	@debate3 = "https://www.youtube.com/embed/-oYIK6Bg6co"
+
   end
+
+  def berniesanders
+  	@candidateName = "Bernie Sanders"
+  	@candidateBiography = "This is his biography!"
+	@speech1 = "https://www.youtube.com/embed/hDRxbQlpqmo" 
+
+  end
+
+
+
 end
