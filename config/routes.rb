58427e4fd6dc2ae75cf08fd68dd7600ea6d53619
michael <mfalicea@buffalo.edu>
2015-10-31 23:41:39 +0000

    I am pretty sure the server is a push behind.  It says it can't find hilaryclinton when it should be hillaryclinton as is in the routes so let's see what happens

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   get '/candidates/berniesanders' => 'candidates#berniesanders'
   get '/candidates/donaldtrump' => 'candidates#donaldtrump'
   get '/candidates/hillaryclinton' => 'candidates#hillaryclinton'
-
+  # Adding this comment because I think that the server is a push behind.
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
