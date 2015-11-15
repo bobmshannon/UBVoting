51ca5d1a619ff193e26364b0973e9e2bcdfe5fea
Desean <deseanab@buffalo.edu>
2015-11-15 02:10:20 +0000

    Carousel Done

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	$('.presidential-candidates').slick({
 	dots: true,
 	lazyLoad: 'ondemand',
-  	slidesToShow: 4,
-  	slidesToScroll: 1
+  	slidesToShow: 6,
+  	slidesToScroll: 1,
+  	autoplay: true,
 });
 });
