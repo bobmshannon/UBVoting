ed8046b7601513d38ac1774579dff4c0054b936d
Bob Shannon <rshannon@buffalo.edu>
2015-11-20 22:20:17 +0000

    Improve candidates dropdown

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -5,6 +5,25 @@ $( document ).ready( function() {
 	// Initialize headroom.js effect on header
 	$( 'header' ).headroom( {
 		offset: 100,
-		tolerance: 10
+		tolerance: 10,
+
+		onUnpin: function() {
+
+			// Close dropdown menu if open
+			$dropdown = $( '.dropdown' );
+			if ( $dropdown.hasClass( 'open' ) ) {
+				$dropdown.removeClass( 'open' );
+				dropdownClosed = true;
+			} else {
+				dropdownClosed = false;
+			}
+		},
+
+		onPin: function() {
+			if ( dropdownClosed ) {
+				$dropdown = $( '.dropdown' );
+				$dropdown.addClass( 'open' );
+			}
+		}
 	} );
 } );
--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,79 +1,78 @@
 /* Homepage specific JS goes here */
-$(document).ready(function() {
-	var ctx = $("canvas#turnout-by-age").get(0).getContext("2d");
+$( document ).ready( function() {
+	var ctx = $( 'canvas#turnout-by-age' ).get( 0 ).getContext( '2d' );
 
 	var data = {
-	    labels: ["January", "February", "March", "April", "May", "June", "July"],
-	    datasets: [
+		labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July' ],
+		datasets: [
 	        {
-	            label: "My First dataset",
-	            fillColor: "rgba(220,220,220,0.2)",
-	            strokeColor: "rgba(220,220,220,1)",
-	            pointColor: "rgba(220,220,220,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(220,220,220,1)",
-	            data: [65, 59, 80, 81, 56, 55, 40]
+		label: 'My First dataset',
+		fillColor: 'rgba(220,220,220,0.2)',
+		strokeColor: 'rgba(220,220,220,1)',
+		pointColor: 'rgba(220,220,220,1)',
+		pointStrokeColor: '#fff',
+		pointHighlightFill: '#fff',
+		pointHighlightStroke: 'rgba(220,220,220,1)',
+		data: [ 65, 59, 80, 81, 56, 55, 40 ]
 	        },
 	        {
-	            label: "My Second dataset",
-	            fillColor: "rgba(151,187,205,0.2)",
-	            strokeColor: "rgba(151,187,205,1)",
-	            pointColor: "rgba(151,187,205,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(151,187,205,1)",
-	            data: [28, 48, 40, 19, 86, 27, 90]
+		label: 'My Second dataset',
+		fillColor: 'rgba(151,187,205,0.2)',
+		strokeColor: 'rgba(151,187,205,1)',
+		pointColor: 'rgba(151,187,205,1)',
+		pointStrokeColor: '#fff',
+		pointHighlightFill: '#fff',
+		pointHighlightStroke: 'rgba(151,187,205,1)',
+		data: [ 28, 48, 40, 19, 86, 27, 90 ]
 	        }
 	    ]
 	};
 
 	var options = {
-	    ///Boolean - Whether grid lines are shown across the chart
-	    scaleShowGridLines : true,
 
-	    //String - Colour of the grid lines
-	    scaleGridLineColor : "rgba(0,0,0,.05)",
+		///Boolean - Whether grid lines are shown across the chart
+		scaleShowGridLines: true,
 
-	    //Number - Width of the grid lines
-	    scaleGridLineWidth : 1,
+		//String - Colour of the grid lines
+		scaleGridLineColor: 'rgba(0,0,0,.05)',
 
-	    //Boolean - Whether to show horizontal lines (except X axis)
-	    scaleShowHorizontalLines: true,
+		//Number - Width of the grid lines
+		scaleGridLineWidth: 1,
 
-	    //Boolean - Whether to show vertical lines (except Y axis)
-	    scaleShowVerticalLines: true,
+		//Boolean - Whether to show horizontal lines (except X axis)
+		scaleShowHorizontalLines: true,
 
-	    //Boolean - Whether the line is curved between points
-	    bezierCurve : true,
+		//Boolean - Whether to show vertical lines (except Y axis)
+		scaleShowVerticalLines: true,
 
-	    //Number - Tension of the bezier curve between points
-	    bezierCurveTension : 0.4,
+		//Boolean - Whether the line is curved between points
+		bezierCurve: true,
 
-	    //Boolean - Whether to show a dot for each point
-	    pointDot : true,
+		//Number - Tension of the bezier curve between points
+		bezierCurveTension: 0.4,
 
-	    //Number - Radius of each point dot in pixels
-	    pointDotRadius : 4,
+		//Boolean - Whether to show a dot for each point
+		pointDot: true,
 
-	    //Number - Pixel width of point dot stroke
-	    pointDotStrokeWidth : 1,
+		//Number - Radius of each point dot in pixels
+		pointDotRadius: 4,
 
-	    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-	    pointHitDetectionRadius : 20,
+		//Number - Pixel width of point dot stroke
+		pointDotStrokeWidth: 1,
 
-	    //Boolean - Whether to show a stroke for datasets
-	    datasetStroke : true,
+		//Number - amount extra to add to the radius to cater for
+		//hit detection outside the drawn point
+		pointHitDetectionRadius: 20,
 
-	    //Number - Pixel width of dataset stroke
-	    datasetStrokeWidth : 2,
+		//Boolean - Whether to show a stroke for datasets
+		datasetStroke: true,
 
-	    //Boolean - Whether to fill the dataset with a colour
-	    datasetFill : true,
+		//Number - Pixel width of dataset stroke
+		datasetStrokeWidth: 2,
 
-	    //String - A legend template
-	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+		//Boolean - Whether to fill the dataset with a colour
+		datasetFill: true
 	};
 
-	var myLineChart = new Chart(ctx).Line(data, options);
-});
+	var myLineChart = new Chart( ctx ).Line( data, options );
+} );
