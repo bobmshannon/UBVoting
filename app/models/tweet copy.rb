f5a6609604650438bde1f86d3a5dae7b652e280a
Bob Shannon <rshannon@buffalo.edu>
2015-12-04 20:57:33 +0000

    Remove unused models

--- app/models/tweet copy.rb
+++
@@ -1,8 +0,0 @@
-class RecentTweeter
-  include Mongoid::Document
-   #User's screen name
-  field :screen_name, type: String
-  #Allowed time to tweet for user
-  field :time, type: Integer  
-
-end
--- app/models/tweet_activity.rb
+++ app/models/tweet_activity.rb
@@ -1,6 +1,6 @@
 class TweetActivity
   include Mongoid::Document
-   #User's screen name
+  # User's screen name
   field :screen_name, type: String
   #Allowed time to tweet for user
   field :time, type: Integer  
