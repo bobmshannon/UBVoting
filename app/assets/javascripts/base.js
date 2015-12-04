f2f78c23d8be61455b3fdab4b4c68f1706d4fc8d
Bob Shannon <rshannon@buffalo.edu>
2015-12-04 02:30:48 +0000

    Cover photo 100% screen height on desktop

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,29 +1,30 @@
 /* Global JS goes in here */
 
 $( document ).ready( function() {
+
 	// Initialize slideout.js mobile style menu
-	var slideout = new Slideout({
-	  'panel': document.getElementById('main'),
-	  'menu': document.getElementById('mobile-menu'),
-	  'padding': 275,
-	  'tolerance': 70,
-	  'side': 'right',
-	  'touch': false
-	});
+	var slideout = new Slideout( {
+		'panel': document.getElementById( 'main' ),
+		'menu': document.getElementById( 'mobile-menu' ),
+		'padding': 275,
+		'tolerance': 70,
+		'side': 'right',
+		'touch': false
+	} );
 
-	slideout.on('beforeopen', function() {
-		$('header').toggleClass('transition-fx');
-	});
+	slideout.on( 'beforeopen', function() {
+		$( 'header' ).toggleClass( 'transition-fx' );
+	} );
 
-	slideout.on('beforeclose', function() {
-		$('header').toggleClass('transition-fx');
-	});
+	slideout.on( 'beforeclose', function() {
+		$( 'header' ).toggleClass( 'transition-fx' );
+	} );
 
-	$('#mobile-menu-toggle').on('click', function() {
+	$( '#mobile-menu-toggle' ).on( 'click', function() {
 		slideout.toggle();
-		$(this).children('i').toggleClass('fa-bars fa-times');
+		$( this ).children( 'i' ).toggleClass( 'fa-bars fa-times' );
 		return false;
-	});
+	} );
 
 	// Initialize headroom.js effect on header
 	$( 'header' ).headroom( {
@@ -31,6 +32,7 @@ $( document ).ready( function() {
 		tolerance: 10,
 
 		onUnpin: function() {
+
 			// Close dropdown menu if open
 			$dropdown = $( '.dropdown' );
 			if ( $dropdown.hasClass( 'open' ) ) {
