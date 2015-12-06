e2c955fda340b15a261b1c7e53f17ef6c78f6564
Bob Shannon <rshannon@buffalo.edu>
2015-12-06 01:14:59 +0000

    Use single generic template for each candidate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,95 +1,46 @@
 class CandidatesController < ApplicationController
 	def berniesanders
 		@candidate = Candidate.find_by(full_name: "Bernie Sanders")
+		render "candidate_template"
 	end
 
 	def donaldtrump
 		@candidate = Candidate.find_by(full_name: "Donald Trump")
+		render "candidate_template"
 	end
 
 	def hillaryclinton
 		@candidate = Candidate.find_by(full_name: "Hillary Clinton")
+		render "candidate_template"
 	end
 
 	def bencarson
 		@candidate = Candidate.find_by(full_name: "Ben Carson")
+		render "candidate_template"
 	end
 
 	def marcorubio
 		@candidate = Candidate.find_by(full_name: "Marco Rubio")
+		render "candidate_template"
 	end
 
 	def martinomalley
 	  	@candidate = Candidate.find_by(full_name: "Martin O'Malley")
+	  	render "candidate_template"
 	end
 
-	def carlyfiorina
-		@candidateName = "Carly Fiorina"
-	   	@lastname = "Fiorina"
-	  	@candidateTitle = "____"
-	  	@affiliation = "democrat"
-	  	@notAffiliated ="republican"
-		@candidateCoverPhoto = "/images/candidates/coverCarly1.jpg"
-		@candidateCoverPhoto1 = "/images/candidates/coverCarly2.jpg"
-
-		@candidateIssueOverview = " "
-		@issue1 = " "
-		@issue1details = " "
-		@issue1Fa = "fa fa-money"
-
-		@issue2 = " "
-		@issue2details
-		@issue2Fa = "fa fa-graduation-cap"
-
-		@issue3 = " "
-		@issue3details = ""
-		@issue3Fa = "fa fa-exclamation-triangle"
-
-		@issue4 = " "
-		@issue4details = " "
-		@issue4Fa = "fa fa-users"
-
-		@issue5 = " "
-		@issue5details = " "
-		@issue5Fa = "fa fa-life-ring"
-
-		@issue6 = " "
-		@issue6details
-		@issue6Fa = "fa fa-cloud"
-
-		@issue7 = " "
-		@issue7details = " "
-		@issue7Fa = "fa fa-star-half-o"
-
-		@issue8 = " "
-		@issue8details
-		@issue8Fa = "fa fa-flag"
-
-		@issue9 = " "
-		@issue9details
-		@issue9Fa = "fa fa-female"
-
-		@candidateQuickBio = " "
-		@candidateBorn = ""
-		@candidateSchool = ""
-		@candidateFamily = ""
-		@candidateStrangeFact1 = ""
-		@candidateStrangeFact2 = ""
-		@candidateStrangeFact3 = ""
-		@candidateAccomplishment1 = ""
-		@candidateAccomplishment2 = ""
-		@candidateAccomplishment3 = ""
-		
-		@source = " "
-
-		@candidateVideo = " "
+    def carlyfiorina
+    	@candidate = Candidate.find_by(full_name: "Carly Fiorina")
+    	render "candidate_template"
 	end
 
 	def tedcruz
 		@candidate = Candidate.find_by(full_name: "Ted Cruz")
+		render "candidate_template"
 	end
 
 	def jebbush
 		@candidate = Candidate.find_by(full_name: "Jeb Bush")
+		render "candidate_template"
 	end
 end
