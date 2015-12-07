be5f7a412ae3e29d72493debd23708448925f035
Desean <deseanab@buffalo.edu>
2015-12-06 20:08:46 -0500

    time interval, div visibility hidden

--- app/assets/javascripts/candidates.js
+++ app/assets/javascripts/candidates.js
@@ -2,23 +2,23 @@
 // All this logic will automatically be available in application.js.
 $( document ).ready(function() {
 	hide(document.querySelectorAll('h3'));
-	
+
     function hide(description) {
       description = description.length ? description : [description];
       for (var i = 0; i < description.length; i++) {
-        description[i].style.display = 'none';
+        description[i].style.visibility = 'hidden';
       }
     };
 
     function show(description){
     	description = description.length ? description: [description];
     	for (var i = 0; i < description.length; i++){
-    		description[i].style.display = 'block';
+    		description[i].style.visibility = 'visible';
     	}
     };
 
     $('i').hover(function(){
-    	show(document.querySelectorAll('h3'));
+    	setTimeout(function(){show(document.querySelectorAll('h3'));}, 900);
     });
 
 } );
