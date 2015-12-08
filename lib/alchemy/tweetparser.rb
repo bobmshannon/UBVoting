fe397ac69937e4255dc2355a57946339eed8cb29
michael <mfalicea@buffalo.edu>
2015-12-08 18:27:16 +0000

    Determining candidate and fecthing from DB and incrementing positive or negative sentiment, it's still a mess

--- lib/alchemy/tweetparser.rb
+++ lib/alchemy/tweetparser.rb
@@ -7,21 +7,35 @@ require './person.rb'
 class TweetParser
 	def initialize()
 		begin
-			@text = "bernie sanders is some guy #feelthebern"
+			@text = "Bernie sanders is the best! and donald trump is the worst"
 			@text.downcase!
 			@gaveSentiment = false 
-			bernie = Candidate.find_by(first_name: "Bernie")
-			puts bernie.first_name
-			bern = Person.new("Bernie Sanders","@berniesanders",
-					     ["#feelthebern","#bernie2016"])
-			trump  = Person.new("Donald Trump","@donaldtrump",
-					       ["#makeamericagreatagain","#trump2016"])
-			clinton  = Person.new("Hillary Clinton","@hillaryclinton",
-						 ["#imwithher","#hillary2016"])
-			@candidates = [bern, trump, clinton]
-
+			initializeCandidates()
+			@candidates = [@sanders, @trump, @clinton, @rubio, 
+		  @omalley,@cruz, @bush, @carson]
 			findCandidate()
 		end
+	end
+
+	#Save some calls to the database
+	#Should be much faster as it's only the data I immediatley need
+	def initializeCandidates()
+		@sanders = Person.new("Bernie Sanders","@berniesanders",
+				      ["#feelthebern","#bernie2016"])
+		@trump  = Person.new("Donald Trump","@donaldtrump",
+				     ["#makeamericagreatagain","#trump2016"])
+		@clinton  = Person.new("Hillary Clinton","@hillaryclinton",
+				       ["#imwithher","#hillary2016"])
+		@rubio = Person.new("Marco Rubio","@marcorubio",
+				    ["#teamrubio","#rubio2016"])
+		@omalley  = Person.new("Martin O'Malley","@martinomalley",
+				       ["#mom2016","#omalley2016"])
+		@cruz  = Person.new("Ted Cruz","@tedcruz",
+				    ["#cruzcrew","#cruz2016"])
+		@bush  = Person.new("Jeb Bush","@jebbush",
+				    ["#","#"])
+		@carson  = Person.new("Ben carson","@realbencarson",
+				      ["#imwithben","#carson2016"])
 	end
 
 
@@ -46,7 +60,6 @@ class TweetParser
 				cand.positiveSentiment += 1
 				puts "Gave #{cand.name} +#{cand.positiveSentiment}" 
 				return 1
-
 			end
 
 		}
@@ -56,7 +69,8 @@ class TweetParser
 
 	def passToAlchemy(text)
 		alchemyapi = AlchemyAPI.new()
-		if (@gaveSentiment == false || @gaveSentiment == false && chosenCandidate != "")
+		if (@gaveSentiment == false || 
+		    @gaveSentiment == false && chosenCandidate != "")
 			response = alchemyapi.entities('text', text, { 'sentiment'=>1 })
 			puts response['entities']
 			puts ' '
@@ -66,16 +80,30 @@ class TweetParser
 					if entity['type'] == 'Person'
 						puts entity['text']
 						puts entity['sentiment']['type']
+						if(entity['sentiment']['type'] == 'positive')
+							foundCandi = Candidate.find_by(full_name_lower: entity['text'])
+							foundCandi.states.find_by(name: "Alabama").positiveSentiment += 1
+							puts "Found #{foundCandi.full_name}"
+							puts "Pos Sentim #{foundCandi.states[0].positiveSentiment}"
+							puts " "
+
+						end
+						if(entity['sentiment']['type'] == 'negative')
+							foundCandi = Candidate.find_by(full_name_lower: entity['text'])
+							foundCandi.states.find_by(name: "Ohio").negativeSentiment += 1
+							puts "Found #{foundCandi.full_name}"
+							puts "Neg Sentim #{foundCandi.states.find_by(name: "Ohio").negativeSentiment}"
+							puts " "
+						end
+
 
 					end
-=begin
-		if entity['type'] == 'Hashtag'
-		puts entity['text']
-end
-if entity['type'] == 'TwitterHandle'
-puts entity['text']
-end
-=end
+					if entity['type'] == 'Hashtag'
+						puts entity['text']
+					end
+					if entity['type'] == 'TwitterHandle'
+						puts entity['text']
+					end
 				end
 			end
 		end
